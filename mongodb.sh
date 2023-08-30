e2538dc87b913d73835ab639df7116e23de6335d
mahipal <mahipal.muchkurr@gmail.com>
2023-08-30 13:32:02 +0530

    changing the listen address and adding the headings

--- mongodb.sh
+++ mongodb.sh
@@ -4,7 +4,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log
 echo -e "\e[36m >>>>>>>>>>>>Installing mongoDB<<<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
 yum install mongodb-org -y &>>$log
 echo -e "\e[36m >>>>>>>>>>>>Changing liste address<<<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongo.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 #Update listen address
 echo -e "\e[36m >>>>>>>>>>>>Enabling and restarting mongodb<<<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
 systemctl enable mongod
