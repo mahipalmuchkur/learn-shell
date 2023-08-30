937695af2aed6ec58c574aeb7b02c0bf8fee247a
mahipal <mahipal.muchkurr@gmail.com>
2023-08-30 13:28:45 +0530

    adding Headings

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ echo -e "\e[36m >>>>>>>>>>>>mongo repo files<<<<<<<<<<<<<<<< \e[0m" | tee -a /tm
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log
 echo -e "\e[36m >>>>>>>>>>>>Installing mongoDB<<<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
 yum install mongodb-org -y &>>$log
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/monogo.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongo.conf
 #Update listen address
 echo -e "\e[36m >>>>>>>>>>>>Enabling and restarting mongodb<<<<<<<<<<<<<<<< \e[0m" | tee -a /tmp/roboshop.log
 systemctl enable mongod
