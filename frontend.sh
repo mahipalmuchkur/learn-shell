b97551694dd79186c1100021f5d75c215dbee73b
mahipal <mahipal.muchkurr@gmail.com>
2023-08-30 13:05:21 +0530

    adding Headings

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,5 @@
 log=/tmp/roboshop.log
-echo -e "\e[31m Installing nginx server \e[0m"
+echo -e "\e[31m Installing nginx server \e[0m"  | tee -a /tmp/roboshop.log
 yum install nginx -y &>>${log}
 echo -e "\e[31m adding conf \e[0m"
 cp frontend.conf /etc/nginx/default.d/roboshop.conf &>>${log}
@@ -8,10 +8,10 @@ systemctl enable nginx
 systemctl start nginx
 echo -e "\e[31m Removing Stuff \e[0m"
 rm -rf /usr/share/nginx/html/*
-echo -e "\e[31m Downloading roboshop content \e[0m"
+echo -e "\e[31m Downloading roboshop content \e[0m"  | tee -a /tmp/roboshop.log
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 cd /usr/share/nginx/html &>>${log}
-echo -e "\e[31m extracting roboshop content \e[0m"
+echo -e "\e[31m extracting roboshop content \e[0m" | tee -a /tmp/roboshop.log
 unzip /tmp/frontend.zip &>>${log}
 echo -e "\e[31m Restarting nginx server \e[0m"
 systemctl restart nginx ; tail -f /var/log/messages
