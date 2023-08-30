491be4967e632c9613f5b0ec3d35d9f04f82cbdc
mahipal <mahipal.muchkurr@gmail.com>
2023-08-30 14:03:09 +0530

    Changing the listen address

--- redis.sh
+++ redis.sh
@@ -1,6 +1,13 @@
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-yum module enable redis:remi-6.2 -y
-yum install redis -y
+log=/tmp/roboshop.log
+echo -e "\e[36m >>>>>>>>>>>>Installing Remirepo<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log}
+echo -e "\e[36m >>>>>>>>>>>>Enabling redis module<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
+yum module enable redis:remi-6.2 -y &>>${log}
+echo -e "\e[36m >>>>>>>>>>>>Changing Listen Address<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log}
+echo -e "\e[36m >>>>>>>>>>>>Installing redis<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
+yum install redis -y &>>${log}
+echo -e "\e[36m >>>>>>>>>>>>Enabling and restart<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
 
