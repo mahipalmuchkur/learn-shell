498d05e2e6f77f8f7b875bdbd07f53a510910934
mahipal <mahipal.muchkurr@gmail.com>
2023-09-01 17:35:39 +0530

    exit status in roboshop

--- common.sh
+++ common.sh
@@ -3,48 +3,58 @@ log=/tmp/roboshop.log
 func_appreq(){
   echo -e "\e[36m >>>>>>>>>>>>Creating ${component} Servie<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
     cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+
      if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
   fi
+
    echo -e "\e[36m >>>>>>>>>>>>Creating Application ${component}<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
     useradd roboshop &>>${log}
+
      if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
   fi
+
     echo -e "\e[36m >>>>>>>>>>>>Cleaning Application Content<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
     rm -rf /app &>>${log}
+
      if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+     fi
     echo -e "\e[36m >>>>>>>>>>>>Creating Application Directory<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
     mkdir /app &>>${log}
+
      if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+     fi
+
     echo -e "\e[36m >>>>>>>>>>>>Download Application Content<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
     curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
      if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+     fi
+
     echo -e "\e[36m >>>>>>>>>>>>Extract Application Content<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
     cd /app &>>${log}
     unzip /tmp/${component}.zip &>>${log}
     cd /app &>>${log}
+
      if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+     fi
 }
 func_systemd(){
     systemctl daemon-reload
@@ -65,13 +75,16 @@ func_nodejs(){
 
   echo -e "\e[36m >>>>>>>>>>>>Installing Nodejs Repo<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+   fi
+
   echo -e "\e[36m >>>>>>>>>>>>Install Nodejs<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   yum install nodejs -y &>>${log}
+
    if [ $? -eq 0]; then
         echo -e "\e[36m SUCCESS \e[0m"
       else
@@ -82,33 +95,41 @@ func_nodejs(){
 
   echo -e "\e[36m >>>>>>>>>>>>Download Nodejs Dependencies<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   npm install &>>${log}
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+   fi
+
   echo -e "\e[36m >>>>>>>>>>>>Installing Mongo Client<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   yum install mongodb-org-shell -y &>>${log}
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+    fi
+
   echo -e "\e[36m >>>>>>>>>>>>Loading Schema<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   mongo --host mongodb.mdevopsb74.online </app/schema/${component}.js &>>${log}
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+   fi
+
   echo -e "\e[36m >>>>>>>>>>>>Daemon-reload Enabling and Restarting<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
 
   func_systemd
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+   fi
+
 }
 
 func_java(){
@@ -116,47 +137,57 @@ func_java(){
 
   echo -e "\e[36m >>>>>>>>>>>>Installing Maven<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   yum install maven -y  &>>${log}
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+   fi
+
   func_appreq
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+   fi
+
   echo -e "\e[36m >>>>>>>>>>>>Cleaning Maven Package<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   mvn clean package  &>>${log}
   echo -e "\e[36m >>>>>>>>>>>>Moving ${component} jar<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   mv target/${component}-1.0.jar ${component}.jar  &>>${log}
   echo -e "\e[36m >>>>>>>>>>>>DaemonReload and enable start<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+   fi
   func_systemd
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+   fi
   echo -e "\e[36m >>>>>>>>>>>>Installing MySQL<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   yum install mysql -y  &>>${log}
-   if [ $? -eq 0 ]; then
+
+  if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
   fi
+
   echo -e "\e[36m >>>>>>>>>>>>online -uroot -pRoboShop@1<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   mysql -h mysql.mdevopsb74.online -uroot -pRoboShop@1 < /app/schema/${component}.sql  &>>${log}
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+   fi
+
   systemctl restart ${component}
 }
 
@@ -164,31 +195,38 @@ func_python(){
 
 echo -e "\e[36m >>>>>>>>>>>>Installing Python<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
 yum install python36 gcc python3-devel -y  &>>${log}
+
+ if [ $? -eq 0 ]; then
+      echo -e "\e[36m SUCCESS \e[0m"
+    else
+      echo -e "\e[31m FAILURE \e[0m"
+ fi
+
+func_appreq
+
  if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
   fi
 
-func_appreq
- if [ $? -eq 0 ]; then
-      echo -e "\e[36m SUCCESS \e[0m"
-    else
-      echo -e "\e[31m FAILURE \e[0m"
-  fi
 echo -e "\e[36m >>>>>>>>>>>>Downloading the Dependencies<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
 pip3.6 install -r requirements.txt  &>>${log}
+
  if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
   fi
+
 echo -e "\e[36m >>>>>>>>>>>>Restarting the Service<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
+
  if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+ fi
+
 func_systemd
 
 }
@@ -197,33 +235,22 @@ func_golang(){
 
   echo -e "\e[36m >>>>>>>>>>>>Installing Golang<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   yum install golang -y   &>>${log}
+
    if [ $? -eq 0 ]; then
       echo -e "\e[36m SUCCESS \e[0m"
     else
       echo -e "\e[31m FAILURE \e[0m"
-  fi
+   fi
 
   func_appreq
 
   echo -e "\e[36m >>>>>>>>>>>>Downloading Dependencies<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
   go mod init dispatch   &>>${log}
-   if [ $? -eq 0 ]; then
-      echo -e "\e[36m SUCCESS \e[0m"
-    else
-      echo -e "\e[31m FAILURE \e[0m"
-  fi
+
   go get   &>>${log}
-   if [ $? -eq 0 ]; then
-      echo -e "\e[36m SUCCESS \e[0m"
-    else
-      echo -e "\e[31m FAILURE \e[0m"
-  fi
+
   go build   &>>${log}
-   if [ $? -eq 0 ]; then
-      echo -e "\e[36m SUCCESS \e[0m"
-    else
-      echo -e "\e[31m FAILURE \e[0m"
-  fi
+
   echo -e "\e[36m >>>>>>>>>>>>Loading the Service<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
 
   func_systemd
