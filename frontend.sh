7faa9f4fa59e517f93301c1a64a8ad4790321506
mahipal <mahipal.muchkurr@gmail.com>
2023-09-01 09:54:09 +0530

    exit status in roboshop

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,17 @@
 log=/tmp/roboshop.log
-echo -e "\e[31m Installing nginx server \e[0m"  | tee -a /tmp/roboshop.log
+echo -e "\e[36m Installing nginx server \e[0m"  | tee -a /tmp/roboshop.log
 yum install nginx -y &>>${log}
-echo -e "\e[31m adding conf \e[0m"
+echo -e "\e[36m adding conf \e[0m"
 cp frontend.conf /etc/nginx/default.d/roboshop.conf &>>${log}
-echo -e "\e[31m Enabling and start \e[0m"
+echo -e "\e[36m Enabling and start \e[0m"
 systemctl enable nginx
 systemctl start nginx
-echo -e "\e[31m Removing Stuff \e[0m"
+echo -e "\e[36m Removing Stuff \e[0m"
 rm -rf /usr/share/nginx/html/*
-echo -e "\e[31m Downloading roboshop content \e[0m"  | tee -a /tmp/roboshop.log
+echo -e "\e[36m Downloading roboshop content \e[0m"  | tee -a /tmp/roboshop.log
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 cd /usr/share/nginx/html &>>${log}
-echo -e "\e[31m extracting roboshop content \e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m extracting roboshop content \e[0m" | tee -a /tmp/roboshop.log
 unzip /tmp/frontend.zip &>>${log}
-echo -e "\e[31m Restarting nginx server \e[0m"
+echo -e "\e[36m Restarting nginx server \e[0m"
 systemctl restart nginx ; tail -f /var/log/messages
