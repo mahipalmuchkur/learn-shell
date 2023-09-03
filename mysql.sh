b816d58e9b5f1aa9e1c7f9ed6c2530958ed90de0
mahipal <mahipal.muchkurr@gmail.com>
2023-09-03 15:02:19 +0530

    Password NFR

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,9 @@
+mysql_root_password=$1
+if[ -z "${mysql_root_password}" ]; then
+  echo Input Password Missing
+  exit 1
+fi
+
 log=/tmp/roboshop.log
 echo -e "\e[36m >>>>>>>>>>>>Creating MySQL Repo<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>${log}
@@ -9,7 +15,7 @@ echo -e "\e[36m >>>>>>>>>>>>Enabling and start mysql<<<<<<<<<<<<<<<< \e[0m"  | t
 systemctl enable mysqld
 systemctl start mysqld  &>>${log}
 echo -e "\e[36m >>>>>>>>>>>>mysql_secure_installation --set-root-pass RoboShop@1<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
-mysql_secure_installation --set-root-pass RoboShop@1  &>>${log}
+mysql_secure_installation --set-root-pass ${mysql_root_password}  &>>${log}
 #mysql -uroot -pRoboShop@1
 echo -e "\e[36m >>>>>>>>>>>>Restart MYSQL<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
 systemctl restart mysqld ; tail -f /var/log/messages  &>>${log}
