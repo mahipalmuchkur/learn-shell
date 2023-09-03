ed9d338933922913ec93d7db69157d4813773ee7
mahipal <mahipal.muchkurr@gmail.com>
2023-09-03 15:28:12 +0530

    Password NFR

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,3 +1,9 @@
+rabbitmq_app_password=$1
+if [ -z "${rabbitmq_app_password}" ]; then
+  echo Input RabbitMQ AppUser Password Missing
+  exit 1
+fi
+
 log=/tmp/roboshop.log
 echo -e "\e[36m >>>>>>>>>>>>Configuring YUM Repos<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log}
@@ -9,5 +15,5 @@ echo -e "\e[36m >>>>>>>>>>>>Start RabbitMQ Service<<<<<<<<<<<<<<<< \e[0m"  | tee
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server &>>${log}
 echo -e "\e[36m >>>>>>>>>>>>create one user for the application<<<<<<<<<<<<<<<< \e[0m"  | tee -a /tmp/roboshop.log
-rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl add_user roboshop ${rabbitmq_app_password}
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log}
